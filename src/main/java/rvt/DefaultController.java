fc3b7bd99539c7cdcf4032e93f0769494239cfe8
vladignatovs <131262207+vladignatovs@users.noreply.github.com>
2024-02-07 07:05:21 +0000

    html com

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -6,21 +6,53 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DefaultController {
-    
-    @GetMapping(value = "/")
-    ModelAndView index(@RequestParam(name="name", required=false, defaultValue="null") String name) {
-        Student student = new Student("John", "Smith", "a@a.lv", "DP2-4");
-        List<Student> students = new ArrayList<>();
-        students.add(student);
 
+    @GetMapping(value = "/")
+    ModelAndView index() {
+        // TODO get students from CSV
+        ArrayList<Student> students = new ArrayList<>();
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("students", students);
         modelAndView.addObject("date", new Date().toString());
         return modelAndView;
     }
+
+    @GetMapping(value = "/test")
+    public ModelAndView testAction() {
+        ModelAndView modelAndView = new ModelAndView("test");
+        modelAndView.addObject("output1", 1);
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/register")
+    public ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView("registration/registration-page");
+        List<String> groups = new ArrayList<>(List.of("DP2-1", "DP2-2", "DP2-3", "DP2-4"));
+        Student student = new Student();
+        modelAndView.addObject("groups", groups);
+        modelAndView.addObject("student", student);
+
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/register")
+    public ModelAndView registerForm(@ModelAttribute("student") Student student) {
+        ModelAndView modelAndView = new ModelAndView("registration/registration-success");
+
+
+        System.out.println(student.getGroup());
+        System.out.println(student.getGender());
+        System.out.println(student.isMarried());
+
+        return modelAndView;
+    }
+
 }
--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -1,16 +1,62 @@
 package rvt;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
-    public String name;
-    public String surname;
-    public String email;
-    public String group;
 
+    @NotEmpty
+    private String name;
 
-    public Student(String name, String surname, String email, String group) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.group = group;
+    @NotEmpty
+    private String surname;
+
+    private int age;
+
+    private String password;
+
+    private LocalDate birthday;
+
+    private String gender;
+
+    private boolean married;
+
+    private String note;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+
+    private String group;
+
+   
+    public String toString() {
+        return String.format(
+                "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
+                this.name,
+                this.surname,
+                this.age,
+                this.password,
+                this.birthday,
+                this.gender,
+                this.married,
+                this.note,
+                this.email,
+                this.group);
     }
+
 }
